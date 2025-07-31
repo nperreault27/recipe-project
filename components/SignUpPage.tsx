1fa4cff1dbbb4efce5dc4fc7942b6cc1e7f018c1
Nicholas Dalsass <nick.dalsass@transperfect.com>
2025-07-31 15:35:01 -0400

    Fixed most of Nick suggested changes from PR, formatter is now single quote :) Form now validates for repeated email and shows all password requirements when blurred

--- components/SignUpPage.tsx
+++ components/SignUpPage.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { createClient } from "@/lib/supabase/client";
+'use client';
+import { createClient } from '@/lib/supabase/client';
 import {
   Button,
   Group,
@@ -8,128 +8,166 @@ import {
   Stack,
   TextInput,
   Title,
-} from "@mantine/core";
-import { isEmail, useForm } from "@mantine/form";
+} from '@mantine/core';
+import { isEmail, useForm } from '@mantine/form';
 
 const SignUpPage = () => {
   const supabase = createClient();
 
   const form = useForm({
-    mode: "uncontrolled",
-    onSubmitPreventDefault: "validation-failed",
+    mode: 'uncontrolled',
+    validateInputOnBlur: true,
+    onSubmitPreventDefault: 'validation-failed',
     initialValues: {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     },
     validate: {
-      name: (value) =>
-        value.length < 2 ? "Name must have at least 2 letters" : null,
-      email: isEmail("Invalid Email"),
+      username: (value) => {
+        if (value.length < 1) return 'Username must have at least 1 character';
+      },
+      email: isEmail('Invalid Email'),
       password: (value) => {
-        if (value.length < 6) return "Password must be at least 6 characters";
-        if (!/[0-9]/.test(value)) return "Must include a number";
-        if (!/[a-z]/.test(value)) return "Must include a lowercase letter";
-        if (!/[A-Z]/.test(value)) return "Must include an uppercase letter";
+        const passwordValidators: string[] = [];
+
+        if (value.length < 6)
+          passwordValidators.push('Minimum of six characters');
+        if (!/[0-9]/.test(value)) passwordValidators.push('One number');
+        if (!/[a-z]/.test(value))
+          passwordValidators.push('One lowercase letter');
+        if (!/[A-Z]/.test(value))
+          passwordValidators.push('One uppercase letter');
         if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(value))
-          return "Must include a special symbol";
+          passwordValidators.push('One special symbol');
         if (value.length > 30)
-          return "Password must be fewer than 30 characters";
-        return null;
+          passwordValidators.push('Maximum of 30 characters');
+
+        return passwordValidators.length > 0
+          ? `Requirements: ${passwordValidators.slice(0).join(', ')}`
+          : null;
       },
       confirmPassword: (value, values) => {
-        if (value !== values.password) return "Passwords do not Match";
+        if (value !== values.password) return 'Passwords do not Match';
       },
     },
   });
 
   const handleSubmit = async (values: {
+    username: string;
     email: string;
     password: string;
-    name: string;
   }) => {
     const { error } = await supabase.auth.signUp({
       email: values.email,
       password: values.password,
       options: {
         data: {
-          display_name: values.name,
+          display_name: values.username,
         },
       },
     });
 
     if (error) {
-      console.error("Signup error: ", error);
+      if (error.message === 'User already registered') {
+        form.setFieldError(
+          'email',
+          'An account with this email already exists'
+        );
+        return;
+      } else {
+        alert('An unknown error occurred during signup');
+      }
     }
 
-    const homePageUrl = "/";
+    const homePageUrl = window.location.origin;
     window.location.href = homePageUrl;
   };
 
   return (
     <>
-      <Paper radius="md" shadow="md" withBorder bg={"#EEEEEE"} p="xl">
-        <Stack gap="xl">
+      <Paper radius='md' shadow='md' withBorder bg={'#EEEEEE'} p='xl'>
+        <Stack gap='xl'>
           <Title order={1}>Sign Up</Title>
 
-          <form method="POST" onSubmit={form.onSubmit(handleSubmit)}>
-            <Stack gap="md" w={300}>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              form.onSubmit(handleSubmit)();
+            }}
+          >
+            <Stack gap='md' w={300}>
               <TextInput
-                name="name"
-                label="Full Name"
+                name='username'
+                label='Username'
                 withAsterisk
-                variant="filled"
-                autoComplete="off"
+                variant='filled'
+                autoComplete='off'
                 required
-                placeholder="Enter your full name..."
-                key={form.key("name")}
-                {...form.getInputProps("name")}
-              />
-
-              <TextInput
-                name="email"
-                label="Email"
-                withAsterisk
-                required
-                variant="filled"
-                autoComplete="off"
                 styles={{
                   input: {
-                    borderColor: "#000000",
+                    borderColor: '#000000',
                   },
                 }}
-                placeholder="foodlover@gmail.com"
-                key={form.key("email")}
-                {...form.getInputProps("email")}
+                placeholder='Enter your full name...'
+                key={form.key('username')}
+                {...form.getInputProps('username')}
+              />
+
+              <TextInput
+                name='email'
+                label='Email'
+                withAsterisk
+                required
+                styles={{
+                  input: {
+                    borderColor: '#000000',
+                  },
+                }}
+                variant='filled'
+                autoComplete='off'
+                placeholder='foodlover@gmail.com'
+                key={form.key('email')}
+                {...form.getInputProps('email')}
               />
 
               <PasswordInput
-                name="password"
-                label="Password"
+                name='password'
+                label='Password'
                 withAsterisk
                 required
-                variant="filled"
-                autoComplete="off"
-                placeholder="Password..."
-                key={form.key("password")}
-                {...form.getInputProps("password")}
+                styles={{
+                  input: {
+                    borderColor: '#000000',
+                  },
+                }}
+                variant='filled'
+                autoComplete='off'
+                placeholder='Password...'
+                key={form.key('password')}
+                {...form.getInputProps('password')}
               />
 
               <PasswordInput
-                name="confirmPassword"
-                label="Confirm Password"
+                name='confirmPassword'
+                label='Confirm Password'
                 withAsterisk
                 required
-                variant="filled"
-                autoComplete="off"
-                placeholder="Confirm Password..."
-                key={form.key("confirmPassword")}
-                {...form.getInputProps("confirmPassword")}
+                styles={{
+                  input: {
+                    borderColor: '#000000',
+                  },
+                }}
+                variant='filled'
+                autoComplete='off'
+                placeholder='Confirm Password...'
+                key={form.key('confirmPassword')}
+                {...form.getInputProps('confirmPassword')}
               />
 
-              <Group justify="center" mt="lg">
-                <Button type="submit" variant="filled" color="#309553">
+              <Group justify='center' mt='lg'>
+                <Button type='submit' variant='filled' color='#309553'>
                   Submit
                 </Button>
               </Group>
