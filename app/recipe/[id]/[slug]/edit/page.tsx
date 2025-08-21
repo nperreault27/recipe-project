ac5318a57f83461aeb0cf0ecaa6b68b373d06495
Nicholas.Perreault <nicholas.perreault@transperfect.com>
2025-08-21 11:58:52 -0400

    made edit page actually serverside rendered by removing use state and use effect

--- app/recipe/[id]/[slug]/edit/page.tsx
+++ app/recipe/[id]/[slug]/edit/page.tsx
@@ -1,16 +1,11 @@
 import { EditRecipe } from '@/components/EditRecipe';
-import React from 'react';
 
-export default function Page({ params }: { params: Promise<{ id: string; slug: string }> }) {
-  const [resolvedParams, setResolvedParams] = React.useState<{ id: string; slug: string } | null>(null);
-
-  React.useEffect(() => {
-    (async () => {
-      setResolvedParams(await params);
-    })();
-  }, [params]);
-
-  if (!resolvedParams) return <div>Loading...</div>;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string; slug: string }>;
+}) {
+  const resolvedParams = await params;
 
   return (
     <main className='min-h-screen flex flex-col items-center'>
