69a8ef28b87b9613ff35e6f6ddcd78ee34d577a1
Robin <robin.juerakhan@transperfect.com>
2025-08-21 11:01:35 -0400

    awaiting linting fix w/ resolved params

--- app/recipe/[id]/[slug]/edit/page.tsx
+++ app/recipe/[id]/[slug]/edit/page.tsx
@@ -1,9 +1,20 @@
 import { EditRecipe } from '@/components/EditRecipe';
+import React from 'react';
 
-export default function Page({ params }: { params: { id: string; slug: string } }) {
+export default function Page({ params }: { params: Promise<{ id: string; slug: string }> }) {
+  const [resolvedParams, setResolvedParams] = React.useState<{ id: string; slug: string } | null>(null);
+
+  React.useEffect(() => {
+    (async () => {
+      setResolvedParams(await params);
+    })();
+  }, [params]);
+
+  if (!resolvedParams) return <div>Loading...</div>;
+
   return (
     <main className='min-h-screen flex flex-col items-center'>
-      <EditRecipe id={params.id} />
+      <EditRecipe id={resolvedParams.id} />
     </main>
   );
 }
